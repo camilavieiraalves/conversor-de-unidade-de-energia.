f322d54ae307f7a8fea70f23844d63e0c288b1e9
Camila Vieira Alves <camilavieiraalves97@gmail.com>
2026-07-16 13:21:09 +0000

    :sparkles: criação do arquivo js

---
+++ script.js
@@ -0,0 +1,9 @@
+const fatores = {
+    "Joule (J)": 1,
+    "Quilojoule (kJ)": 1000,
+    "Caloria (cal)": 4.184,
+    "Quilocaloria (kcal)": 4184,
+    "Watt-hora (Wh)": 3600,
+    "Quilowatt-hora (kWh)": 3600000,
+    "Elétron-volt (eV)": 1.602176634e-19
+};
